eeb829ee213c881e4f24087ac347cbcec23358e6
Anders Fisher <atleastimtrying@gmail.com>
2012-12-24 10:00:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheAbyss::Application.config.secret_token = '8a608fa128006af6cf6177c3246dfc701a304d737485a30a8eb900e37593b1103aee37e4cd6357056b22714aa2adb5dd0b8b75f3554882706a2631f4d02ab8b5'
