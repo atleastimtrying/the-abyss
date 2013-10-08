676709b42d830ceeb9d6fc4e225c1d6ae213632a
Anders Fisher <atleastimtrying@gmail.com>
2013-10-08 21:32:55 +0100

    more scopes for plots and an improved admin dashboard

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,3 +1,5 @@
 class Plot < ActiveRecord::Base
   scope :published, -> { where published: true }
+  scope :unpublished, -> { where published: false }
+  scope :pending, -> { unpublished.order('created_at DESC') }
 end
