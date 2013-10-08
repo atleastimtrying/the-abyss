627a6f21d94e5407615a7a29aa6f9f706b893cf5
Anders Fisher <atleastimtrying@gmail.com>
2013-10-08 21:17:56 +0100

    bulk publish/unpublish functionality

--- app/admin/plot.rb
+++ app/admin/plot.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Plot do
   index do
+    selectable_column
     column :title
     column :x
     column :y
@@ -8,6 +9,20 @@ ActiveAdmin.register Plot do
   end
 
   filter :published
+
+  batch_action :publish do |selection|
+    Plot.find(selection).each do |plot|
+      plot.update_attributes published: true
+    end
+    redirect_to collection_path, notice: "published"
+  end
+
+  batch_action :unpublish do |selection|
+    Plot.find(selection).each do |plot|
+      plot.update_attributes published: false
+    end
+    redirect_to collection_path, alert: "unpublished"
+  end
 
   controller do
     def permitted_params
